7745ab4d87f8496acaab9c6ed6135e128e3e7300
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-13 12:05:25 +0100

    adding dummy text to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,6 +10,13 @@ export default function AboutPage() {
 				<meta key="description" name="description" content="About" />
 			</Head>
 			<h1>About</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+				dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+				nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+				quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
+				nec, vulputate
+			</p>
 		</Layout>
 	);
 }

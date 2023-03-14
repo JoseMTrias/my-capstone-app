17b3bfdcce5f91661dd9cb7162a5eeeaff852493
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-14 12:26:05 +0100

    fixing lint problems on button and footer

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -4,11 +4,11 @@ import {pxToRem} from '../../utils/unit';
 
 const StyledButton = styled.button`
 	padding: 0.5em 1em;
+	margin: 5px;
 	border: 1px solid rgba(0, 0, 0, 0.3);
 	border-radius: ${pxToRem(4)};
 	background: lightgray;
 	color: #000;
-	margin: 5px;
 	font-size: 1em;
 
 	&:hover {
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -3,17 +3,17 @@ import styled from 'styled-components';
 import {pxToRem} from '../../utils/unit';
 
 const StyledFooter = styled.footer`
+	display: flex;
+	position: fixed;
+	justify-content: right;
 	padding: 0.5em 1em;
 	border: 1px solid rgba(0, 0, 0, 0.3);
 	border-radius: ${pxToRem(4)};
 	background: lightgray;
 	color: #000;
-	display: flex;
 	font-size: 1em;
-	justify-content: right;
-	position: fixed;
-	width: 100%;
 	bottom: 0;
+	width: 100%;
 	&:hover {
 		background: #d8b;
 	}

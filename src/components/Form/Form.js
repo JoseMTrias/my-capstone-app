d241800795abc74c6cf275f7373e7384a605a9d2
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-14 16:54:57 +0100

    displaying the announces from db

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,62 +1,61 @@
-// import React from 'react';
-// import {useState} from 'react';
-import useSWR from 'swr';
+import {useState} from 'react';
 
 import StyledForm from './styled';
 
 export default function Form() {
-	const announces = useSWR('/api/announces');
-
+	const [announcements, setAnnouncements] = useState([]);
+	const [formInfo, setFormInfo] = useState({});
 	const handleSubmit = async event => {
 		event.preventDefault();
 
 		const instrument = event.target.instrument.value;
 		const genre = event.target.genre.value;
 		const location = event.target.location.value;
-		const formData = {
+		setFormInfo({
 			instrument,
 			genre,
 			location,
-		};
-		console.log(formData);
-
-		const response = await fetch('/api/announces', {
-			method: 'POST',
-			body: JSON.stringify(formData),
-			headers: {
-				'Content-Type': 'application/json',
-			},
 		});
-		if (response.ok) {
-			await response.json();
-			announces.mutate();
-			event.target.reset();
-		}
+
+		const response = await fetch('/api/announces');
+		const allAnnouncements = await response.json();
+		setAnnouncements(allAnnouncements);
 	};
 	return (
-		<StyledForm onSubmit={handleSubmit} action="/result" method="post">
-			<label htmlFor="instrument">Instrument:</label>
-			<select className="select" id="instrument" name="instrument">
-				<option value="all_instruments">All</option>
-				<option value="guitar">Guitar</option>
-				<option value="bass">Bass</option>
-				<option value="drums">Drums</option>
-				<option value="vocals">Vocals</option>
-			</select>
-			<label htmlFor="genre">Genre:</label>
-			<select id="genre" name="genre">
-				<option value="all_genres">All</option>
-				<option value="rock">Rock</option>
-				<option value="shoegaze">Shoegaze</option>
-				<option value="pop">Pop</option>
-				<option value="jazz">Jazz</option>
-				<option value="funk">Funk</option>
-				<option value="electronic">Electronic</option>
-				<option value="classical">Classical</option>
-			</select>
-			<label htmlFor="location">Location:</label>
-			<input type="text" id="location" name="location"></input>
-			<button type="submit">Search</button>
-		</StyledForm>
+		<>
+			<StyledForm onSubmit={handleSubmit} action="/result" method="post">
+				<label htmlFor="instrument">Instrument:</label>
+				<select className="select" id="instrument" name="instrument">
+					<option value="All">All</option>
+					<option value="Guitar">Guitar</option>
+					<option value="Bass">Bass</option>
+					<option value="Drums">Drums</option>
+					<option value="Vocals">Vocals</option>
+				</select>
+				<label htmlFor="genre">Genre:</label>
+				<select id="genre" name="genre">
+					<option value="All">All</option>
+					<option value="Rock">Rock</option>
+					<option value="Shoegaze">Shoegaze</option>
+					<option value="Pop">Pop</option>
+					<option value="Jazz">Jazz</option>
+					<option value="Funk">Funk</option>
+					<option value="Electronic">Electronic</option>
+					<option value="Classical">Classical</option>
+				</select>
+				<label htmlFor="Location">Location:</label>
+				<input type="text" id="location" name="location"></input>
+				<button type="submit">Search</button>
+			</StyledForm>
+			{announcements.map(announcement => {
+				if (
+					announcement.instrument === formInfo.instrument ||
+					announcement.genre === formInfo.genre ||
+					announcement.location === formInfo.location
+				) {
+					return <p key={announcement._id}>trial</p>;
+				}
+			})}
+		</>
 	);
 }
--- src/pages/api/announces/index.js
+++ src/pages/api/announces/index.js
@@ -1,17 +1,12 @@
 import dbConnect from '../../../backend/connect';
 import Announce from '../../../backend/models/Announce';
 
-export default async function handler(request) {
+export default async function handler(request, response) {
 	await dbConnect();
 
-	if (request.method === 'POST') {
-		console.log('post as: ', request.body);
+	if (request.method === 'GET') {
 		//get all announcements
 		const announces = await Announce.find();
-		//once you have the announcements filter through depending on the info from the form
-		console.log('announces: ', announces);
-		// return that data to the frontend
+		response.status(200).json(announces);
 	}
 }
-
-// index from pages/api

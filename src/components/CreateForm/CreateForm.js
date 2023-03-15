795ecb47a6b09baffb4aade4a4a7e3102ca26048
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-15 14:19:26 +0100

    adding CreateForm and PUT method

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -1,3 +1,41 @@
 export default function CreateForm() {
-	return <div>CreateForm</div>;
+	return (
+		<>
+			<form>
+				<label>
+					Title:
+					<input type="text" name="title" />
+				</label>
+				<br></br>
+				<label>
+					Date:
+					<input type="text" name="date" />
+				</label>
+				<br></br>
+				<label>
+					Instrument:
+					<input type="text" name="instrument" />
+				</label>
+				<br></br>
+				<label>
+					Genre:
+					<input type="text" name="genre" />
+				</label>
+				<br></br>
+				<label>
+					Location:
+					<input type="text" name="location" />
+				</label>
+				<br></br>
+				<label>
+					User:
+					<input type="text" name="user" />
+				</label>
+				<p>Description:</p>
+				<textarea></textarea>
+				<br></br>
+				<input type="submit" value="Create" />
+			</form>
+		</>
+	);
 }

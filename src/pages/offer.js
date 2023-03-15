b7a319a09d1e4d483114e0779129177fa31a22d2
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-15 13:56:10 +0100

    creating CreateForm

--- src/pages/offer.js
+++ src/pages/offer.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import CreateForm from '../components/CreateForm/CreateForm';
 // import Layout from '../components/Layout/Layout';
 
 export default function OfferPage() {
@@ -10,13 +11,7 @@ export default function OfferPage() {
 				<meta key="description" name="description" content="Offer" />
 			</Head>
 			<h2>Offer</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-				dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-				nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-				quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
-				nec, vulputate
-			</p>
+			<CreateForm></CreateForm>
 		</>
 	);
 }

cef0592eee38e386cfb4ce7bd2483402d555b1f7
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-15 16:41:46 +0100

    adding id.js

---
+++ src/pages/api/announcements/[id].js
@@ -0,0 +1,17 @@
+import dbConnect from '../../../backend/connect';
+import Announcement from '../../../backend/models/Announcement';
+
+export default async function handler(request, response) {
+	await dbConnect();
+
+	const {id} = request.query;
+
+	if (request.method === 'GET') {
+		const announcement = await Announcement.findById(id);
+
+		if (!announcement) {
+			return response.status(404).json({status: 'Not Found'});
+		}
+		response.status(200).json(announcement);
+	}
+}

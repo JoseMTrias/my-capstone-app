63ba14c0d69374be86db415d8ef5bcd23614cfaa
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-22 15:18:46 +0100

    getting filtered announcements log

--- src/pages/users/[id].js
+++ src/pages/users/[id].js
@@ -5,43 +5,28 @@ import { useState, useEffect } from 'react'
 
 export default function profilePage() {
   const { data: session, status } = useSession()
-  console.log("sessssion: ", session)
   const [announcements, setAnnouncements] = useState([])
-  const [userInfo, setUserInfo] = useState(session)
-  const [filteredAnnouncements, setFilteredAnnouncements] = useState()
   
   useEffect(() => {
-    
     async function getAnnouncements() {
      try {
        const announcementsData = await fetch("/api/announcements")
        const announcements = await announcementsData.json()
        setAnnouncements(announcements)
-       
      } catch (error) {
        console.log(error)
      }
    }
    getAnnouncements()
-   
-   
   }, [])
-  console.log("ann:", announcements)
-  
-  useEffect(() => {
-    
+
     const filteredAnnouncements = announcements.filter((announcement) => {
-      if (session.user.id === announcement.userId) {
+      if (session?.user.id === announcement.userId) {
         return announcement
       }
     })
-    setFilteredAnnouncements(filteredAnnouncements)
-  }, [userInfo])
 
   console.log("filtered: ", filteredAnnouncements)
-
-
-
 
   if (status === "authenticated") {
     return (
@@ -49,7 +34,9 @@ export default function profilePage() {
         <StyledCard>
           <p>User: {session.user.name}</p>
           <p>Email: {session.user.email}</p>
-
+          <ul>
+            <li>{filteredAnnouncements.title}</li>
+          </ul>
         </StyledCard>
       </>
     )

83742a3d8ad753ac4c651199395461b624921583
JoseMTrias <jmgarciatrias@gmail.com>
2023-03-30 13:34:38 +0200

    adding p to locations

--- src/pages/map.js
+++ src/pages/map.js
@@ -4,6 +4,7 @@ export default function MapPage() {
     return (
     <>
     <h3 className="map-header">Locations</h3>
+    <p>Our favourite collection of concert venues and rehearsal rooms in Berlin!</p>
     <Map></Map>
     </>
     )
